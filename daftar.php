6346c14ef48b838a8671e55446720f8be7b1c45b
simonsmjtk270904 <simonsimanjuntak0904@gmail.com>
2025-01-12 18:15:25 +0700

    Website Coffe Kenagan Mantan

---
+++ daftar.php
@@ -0,0 +1,7 @@
+<form action="process_booking.php" method="POST">
+    <input type="text" name="name" placeholder="Name" class="box" required>
+    <input type="email" name="email" placeholder="Email" class="box" required>
+    <input type="number" name="number" placeholder="Number" class="box" required>
+    <textarea name="message" placeholder="Message" class="box" cols="30" rows="10" required></textarea>
+    <input type="submit" value="Send Message" class="btn">
+</form>
